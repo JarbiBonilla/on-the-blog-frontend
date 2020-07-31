0ed00b38ea90f393c83e57eeb20c1ddb1b74a588
JarbiBonilla <jarbibonilla@gmail.com>
2020-07-31 01:12:29 -0400

    built blog reducer

---
+++ src/reducers/BlogsReducer.js
@@ -0,0 +1,15 @@
+export default (state = [], action) => {
+    switch(action.type) {
+        case "GET_BLOGS":
+            return [...state, action.payload]
+
+        case "ADD_BLOG":
+            return [...state, action.payload]
+
+        case "REMOVE_BLOG":
+            return state.filter(blog => blog.id !== action.payload)
+
+        default: 
+            return state
+    }
+}
