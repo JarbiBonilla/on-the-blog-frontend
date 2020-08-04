f02ac18b160594641ce2496beb6a00c94779db67
JarbiBonilla <jarbibonilla@gmail.com>
2020-08-04 16:24:05 -0400

    working on home container for blogs

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div id="home-container">
+                <h1>Welcome To On The Blog!</h1>
+
+            </div>
+        )
+    }
+
+
+
+
+}
