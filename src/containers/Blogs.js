ff5a7ffb663084e38d6248f54280a536aea12404
JarbiBonilla <jarbibonilla@gmail.com>
2020-08-04 17:06:06 -0400

    created a blogs container and imported everything

---
+++ src/containers/Blogs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import BlogCard from '../components/BlogCard'
+import { connect } from 'react-redux'
+import AddBlogButton from '../components/AddBlogButton'
+import { fetchBlogs, addBlog} from '../actions/index'
+
+
+class Blogs extends Component {
+
+
+
+
+
+    
+}
