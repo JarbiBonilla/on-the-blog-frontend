6e4172c628cb37e629dbe864e723c582cd708776
JarbiBonilla <jarbibonilla@gmail.com>
2020-08-04 17:14:54 -0400

    created render method, mapstatetoprops, and mapdispatchtoprops in car container file

--- src/containers/Blogs.js
+++ src/containers/Blogs.js
@@ -7,9 +7,31 @@ import { fetchBlogs, addBlog} from '../actions/index'
 
 class Blogs extends Component {
 
+    componentDidMount() {
+        this.props.fetchBlogs()
+    }
 
-
-
-
-    
+    render() {
+        return(
+          <div className="blogs-container">
+            < AddBlogButton name="Create Your Blog" addBlog={this.props.addBlog}/>
+            {this.props.blogs.map((b, id) => < BlogCard key={id} blog={b} />)}
+          </div>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+      blogs: state.blogs
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+      fetchBlogs: () => dispatch(fetchBlogs()),
+      addBlog: blog => dispatch(addBlog(blog))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blogs)
