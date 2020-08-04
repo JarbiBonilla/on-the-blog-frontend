62388a50b4b92fed6c30451a7093a47ce1f3c92b
JarbiBonilla <jarbibonilla@gmail.com>
2020-08-04 16:01:06 -0400

    created second fetch call for adding a blog in my actions index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,32 @@
 // EX: GET localhost:3000/blogs
 
 
-export const fetchBlogPosts = () => {
+export const fetchBlogs = () => {
     return dispatch => {
-      return fetch(`http://localhost:3001/blogs`)
+      return fetch(`http://localhost:3000/blogs`)
         .then(res => res.json())
         .then(blogs => {
           dispatch({type: "GET_BLOGS", payload: blogs})
+        })
+    }
+  }
+
+  // second fetch call. this will add a new blog
+  // EX: POST localhost:3000/blogs
+
+  export const addBlog = blog => {
+    return dispatch => {
+      return fetch(`http://localhost:3001/blogs`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({blog: blog})
+      })
+        .then(res => res.json())
+        .then(blog => {
+  
+          dispatch({type: "ADD_BLOG", payload: blog})
         })
     }
   }
